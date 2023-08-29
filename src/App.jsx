890adbe9ae5bb927b72a2ca88d94702c98b4986e
SamM71 <sammocock@gmail.com>
2023-08-29 15:34:04 +0100

    add Info

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Info() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1>The Kooks</h1>
+      <div className="info">
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Genre: Pop-rock
+        </p>
+        <p>
+          The Kooks are an English pop-rock band formed in 2004 in Brighton. The band currently consists of Luke Pritchard (vocals/rhythm guitar), Hugh Harris (lead guitar/synthesizer/bass) and Alexis Nunez (drums).
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
+}
+
+function App() {
+  return (
+    <Info />
+  )
+
 }
 
 export default App
